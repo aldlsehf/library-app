930e47930786a33f2788e142802c3828c6449066
aldlsehf <minseok5027@cau.ac.kr>
2024-03-26 16:03:38 +0900

    first commit

---
+++ src/main/java/com/group/libraryapp/dto/book/request/BookReturnRequest.java
@@ -0,0 +1,22 @@
+package com.group.libraryapp.dto.book.request;
+
+public class BookReturnRequest {
+    private String userName;
+    private String bookName;
+
+    public BookReturnRequest() {
+    }
+
+    public BookReturnRequest(String userName, String bookName) {
+        this.userName = userName;
+        this.bookName = bookName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+}
